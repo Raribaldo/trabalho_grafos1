3f35590740c3ddf4ae1c280dd1d6592a8bf5ebc4
João Ricardo Santiago Magalhães <146662740+Joao-Ricardo-S-Magalhaes@users.noreply.github.com>
2023-10-01 18:32:09 -0300

    Update representacao.py

--- representacao.py
+++ representacao.py
@@ -1,44 +1,44 @@
 class ListaAdjacencia:
     def __init__ (self, n_vertices, arestas):
-        self.n_arestas = len(arestas)
-        self.nodes = []
+        self.n_arestas = len(arestas) # numero de arestas #
+        self.nodes = [] # lista com os nós #
         for K in range (n_vertices):
-            self.nodes.append([])
-        for aresta in arestas:
-            (inicio, fim) = aresta
-            self.nodes[inicio].append(fim)
+            self.nodes.append([]) # lista com os índices dos vértices #
+        for aresta in arestas: # declaração das arestas #
+            (inicio, fim) = aresta 
+            self.nodes[inicio].append(fim) 
             self.nodes[fim].append(inicio)
-    def get_arestas(self, node):
-        return self.nodes[node]
+    def get_arestas(self, node): 
+        return self.nodes[node] # retorna as arestas do nó #
     
     def n_nodes (self):
-        return len(self.nodes)
+        return len(self.nodes) # número de vértices #
     
 class MatrixAdjacencia:
     def __init__ (self, n_vertices, arestas):
-        self.n_arestas = len(arestas)
-        self.nodes = []
+        self.n_arestas = len(arestas) # numero de arestas #
+        self.nodes = [] # lista com os nós #
         for i in range (n_vertices):
             matrix = []
             for j in range (n_vertices):
                 matrix.append(0)
             self.nodes.append(matrix)
         
-        for aresta in arestas:
+        for aresta in arestas: # declaração das arestas #
             (inicio, fim) = aresta
-            self.nodes[inicio][fim] = 1
-            self.nodes[fim][inicio] = 1
-    def get_arestas(self, node):
-        nos = []
+            self.nodes[inicio][fim] = 1 # define em 1 se dois vértices estiverem ligados #
+            self.nodes[fim][inicio] = 1 # define em 1 se dois vértices estiverem ligados #
+    def get_arestas(self, node): # retorna as arestas do nó #
+        nos = [] # lista com os vértices #
         index = 0
-        for i in self.nodes[node]:
+        for i in self.nodes[node]: #
             if i != 0:
                 nos.append(index)
             index += 1
         return nos
     
     def n_nodes (self):
-        return len(self.nodes)
+        return len(self.nodes) # número de vértices #
     
 
         
